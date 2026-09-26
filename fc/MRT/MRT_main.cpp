c049806fa25ad0fcdb564d0a192720ef2ddfce96
Aneurin Hullah <aneurin.hullah+github@gmail.com>
2026-09-26 12:52:34 -0400

    Create MRT setup file

--- fc/MRT/MRT_main.cpp
+++ fc/MRT/MRT_main.cpp
@@ -2,31 +2,10 @@
 #include "gpio.h"
 #include "cmsis_os.h"
 
+#include <radios/AtomicRules.h>
+
 // Called on Telemetry thread
-void blinky(){
+void MRT_main(){
 
-    volatile int test = 0;
-    volatile bool doLED1 = false;
-    GPIO_TypeDef* port = OUT_LED2_GPIO_Port;
-    int pin = OUT_LED2_Pin;
-    while(1){
-        if(doLED1){
-            port = OUT_LED1_GPIO_Port;
-            pin = OUT_LED1_Pin;
-        } else {
-            port = OUT_LED2_GPIO_Port;
-            pin = OUT_LED2_Pin;
-        }
-
-
-        HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
-
-        osDelay(500);
-
-        test++;
-
-        HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
-        
-        osDelay(500);
-    }
+    test_atomic_rules();
 }
--- fc/MRT/MRT_main.h
+++ fc/MRT/MRT_main.h
@@ -1,11 +1,21 @@
 #pragma once 
+#include <util/HAL.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void blinky();
+void MRT_main();
 
 #ifdef __cplusplus
 }
 #endif
+
+
+/* Thread Plan:
+    Watchdog
+    Commands
+    Recovery
+    Telemetry
+    CAN (But better, without crashing program)
+*/
